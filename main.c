2b573fb00f3658be97a9709fc919c95b8c45cb33
shybu8 <bushink04@proton.me>
2026-03-15 10:27:42 +0300

    Serialization added, but realloc heavy

---
+++ main.c
@@ -0,0 +1,56 @@
+#include "json.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(void) {
+  FILE *f = fopen("example.json", "r");
+  fseek(f, 0, SEEK_END);
+  size_t f_size = ftell(f);
+  fseek(f, 0, SEEK_SET);
+  char *f_alloc = malloc(f_size);
+  char *f_content = f_alloc;
+  fread(f_content, sizeof(char), f_size, f);
+
+  JsonVal val;
+
+  float st = (float)clock() / CLOCKS_PER_SEC;
+  json_parse_val(&val, (const char **)&f_content);
+  float et = (float)clock() / CLOCKS_PER_SEC;
+  float dt = et - st;
+  printf("Deserialization delta time is %f\n", dt);
+
+  // printf("Value is %d\n", val.type);
+  // if (val.type == JSON_TYPE_OBJ) {
+  //   JsonVal *found = json_value_by_key(val.as.obj_ptr, "1");
+  //   if (found && found->type == JSON_TYPE_STR)
+  //     printf("%.*s\n", (int)found->as.str_ptr->len,
+  //     found->as.str_ptr->start);
+  // }
+
+  st = (float)clock() / CLOCKS_PER_SEC;
+  char *buf = malloc(1);
+  *buf = '\0';
+  size_t buf_len = strlen(buf);
+  JsonStyle style = JSON_STYLE_PRETTY_PRINT_FOURSPACES;
+  json_serialize_val(&val, &buf, &buf_len, &style);
+  et = (float)clock() / CLOCKS_PER_SEC;
+  dt = et - st;
+  printf("Serialization delta time is %f\n", dt);
+  // printf("%s\n", buf);
+  printf("%ld\n", strlen(buf));
+  free(buf);
+
+  st = (float)clock() / CLOCKS_PER_SEC;
+  json_free_val(&val);
+  et = (float)clock() / CLOCKS_PER_SEC;
+  dt = et - st;
+  printf("Freeing delta time is %f\n", dt);
+
+  free(f_alloc);
+
+  return 0;
+}
